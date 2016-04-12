21b3e29476fcf7435f296f45a3906733880a4011
paulcartmel <paul@newmedialabs.co.za>
2016-04-12 16:26:27 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,9 @@
+﻿var http = require('http');
+var port = process.env.port || 1337;
+
+
+http.createServer(function (request, response) {
+    response.writeHead(200, { 'Content-Type': 'text/plain' });
+    response.end('Paul Play, Server Started\n');
+}).listen(port);
+
