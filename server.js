8080c5154263b8ad98e13709ad1120316ad9b7de
paulcartmel <paul@newmedialabs.co.za>
2016-04-12 16:52:53 +0200

    just testing something

--- server.js
+++ server.js
@@ -5,5 +5,6 @@ var port = process.env.port || 1337;
 http.createServer(function (request, response) {
     response.writeHead(200, { 'Content-Type': 'text/plain' });
     response.end('Paul Play, Server Started\n');
+    response.end('Paul Play, Server Started\n');
 }).listen(port);
 
